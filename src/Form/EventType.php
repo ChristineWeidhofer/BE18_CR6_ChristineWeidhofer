a18eebdf275522d09583aadc95454285e2ded9a2
ChristineWeidhofer <xtine@gmx.at>
2023-04-14 10:06:33 +0200

    make:crud

---
+++ src/Form/EventType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('date')
+            ->add('descr')
+            ->add('image')
+            ->add('cap')
+            ->add('location')
+            ->add('email')
+            ->add('phone')
+            ->add('url')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
